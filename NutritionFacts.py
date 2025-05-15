ba36195ec1bcf8771f04e5f659d07e4306c2f28b
Krisha052 <165743536+Krisha052@users.noreply.github.com>
2025-05-14 21:41:16 -0400

    Add files via upload

---
+++ NutritionFacts.py
@@ -0,0 +1,31 @@
+def main():
+    calories_by_fruit = {
+        "apple": 130,
+        "avocado": 50,
+        "banana": 110,
+        "cantaloupe": 50,
+        "grapefruit": 60,
+        "grapes": 90,
+        "honeydew melon": 50,
+        "kiwifruit": 90,
+        "lemon": 15,
+        "lime": 20,
+        "nectarine": 60,
+        "orange": 80,
+        "peach": 60,
+        "pear": 100,
+        "pineapple": 50,
+        "plums": 70,
+        "strawberries": 50,
+        "sweet cherries": 100,
+        "tangerine": 50,
+        "watermelon": 80
+    }
+
+    item = input("Item: ").lower().strip()
+    if item in calories_by_fruit:
+        print(f"Calories: {calories_by_fruit[item]}") #If do nothing then no need to use the except block in this situation
+
+
+if __name__ == "__main__":
+    main()
